42744e91ad7cb27c5ac2b919ae89905e793b2ecf
Alexandr Derevyanko <7171sites@gmail.com>
2023-05-10 16:56:40 +0500

    Initial Commit

---
+++ SwiftUIHomework/SwiftUIHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHomeworkApp.swift
+//  SwiftUIHomework
+//
+//  Created by Aleksandr Derevyanko on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
